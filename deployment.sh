43fd07919e4a83dd5fdd036ddaf6e13abf501954
caiwillie <caiwl@shinemo.com>
2020-10-22 20:23:44 +0800

    update

--- deployment.sh
+++ deployment.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env sh
 
-jekyll build --baseurl /jekyll-asciidoc-quickstart --destination ~/code/github/caiwillie/caiwillie.github.io
+jekyll build --baseurl / --destination ~/code/github/caiwillie/caiwillie.github.io
+cd ~/code/github/caiwillie/caiwillie.github.io
+git add --all
+git commit -m "update"
+git push
